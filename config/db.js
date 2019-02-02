4d611a2c818a8ce4478de50e8aecdfce71f70b44
Khushboo Sakervala <ksakerva@my.centennialcollege.ca>
2019-02-02 12:56:30 -0500

    added mlab support

--- config/db.js
+++ config/db.js
@@ -2,4 +2,5 @@ module.exports = {
   // local MongoDB deployment
 
   URI: "mongodb://localhost/demo"
+  //URI: "mongodb://<dbuser>:<dbpassword>@ds119755.mlab.com:19755/week4"
 };
--- routes/contact.js
+++ routes/contact.js
@@ -1,22 +1,24 @@
 let express = require("express");
 let router = express.Router();
+let mongoose = require("mongoose");
 
 // create a reference to the contact model
 let contact = require("../models/contact");
 
-/* GET contact list page - read*/
+/* GET contact list page - READ */
 router.get("/", (req, res, next) => {
-  contact.find((err, contactsList) => {
+  contact.find((err, contactList) => {
     if (err) {
       return console.error(err);
     } else {
-      console.log(contactsList);
-      /*
-            res.render('contacts/index',{
-                title: 'Contacts List',
-                contactsList: contactList
-            });*/
+      console.log(contactList);
+
+      res.render("contacts/index", {
+        title: "Contact List",
+        contactList: contactList
+      });
     }
   });
 });
+
 module.exports = router;
